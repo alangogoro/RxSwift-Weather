2a0b409449e73f99084b88b87a5ab076b232be0c
alangogoro <cayne9160@gmail.com>
2021-08-12 12:33:55 +0800

    Install RxSwift

--- Models/Weather.swift
+++ Models/Weather.swift
@@ -15,3 +15,5 @@ struct Weather: Decodable {
 struct WeahterData: Decodable {
     let main: Weather
 }
+
+let api_sample = "https://api.openweathermap.org/data/2.5/weather?q=tokyo&lang=ja&APPID=9aa8eb47ca75209cd4c3bccc88045531"
