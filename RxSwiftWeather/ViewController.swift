15f35ef29a85e27cdfb8b249d50e72e54e620def
alangogoro <cayne9160@gmail.com>
2021-08-12 12:09:23 +0800

    UI setup

--- RxSwiftWeather/ViewController.swift
+++ RxSwiftWeather/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Properties
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
