5c661cef3df8560a3313efc7cdfae921f3062d85
alangogoro <cayne9160@gmail.com>
2021-08-13 11:58:12 +0800

    test Commit

--- Extensions/URLRequest+Extensions.swift
+++ Extensions/URLRequest+Extensions.swift
@@ -39,14 +39,14 @@ extension URLRequest {
     // 限制參數需遵從 Decodable 協定
     /* static func load<T: Decodable>(resource: Resource<T>)
     -> Observable<T> {
-        // TODO: WHAT IS THIS
+        
         return Observable.from([resource.url])
             .flatMap { url -> Observable<Data> in
                 let request = URLRequest(url: url)
                 return URLSession.shared.rx.data(request: request)
-                    // 此處 return 一個 Observable<Data>
+                // 此處 return 一個 Observable<Data>
             }.map { data -> T in
-                        // 將上方的 Observable<Data> 解碼成自訂的型別 (WeatherData, Weather)
+                // 將上方的 Observable<Data> 解碼成自訂的型別 (WeatherData, Weather)
                 return try JSONDecoder().decode(T.self, from: data)
             }.asObservable()
         
